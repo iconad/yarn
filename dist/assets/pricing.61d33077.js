cc2cf3248efc2607421661507816d6807725c7f4
Amir <amir@icon-ad.com>
2023-01-27 12:26:42 +0400

    spacing fixed and blog paged updated

--- dist/assets/pricing.61d33077.js
+++
@@ -1,1 +0,0 @@
-import{S as a}from"./global.fbd64f69.js";new a(".splide",{perPage:1,gap:30,pagination:!1}).mount();
---
+++ dist/assets/pricing.94d24fa8.js
@@ -0,0 +1,1 @@
+import{S as a}from"./global.37da93a8.js";new a(".pricingsplide",{perPage:1,gap:0,pagination:!0,arrows:!1}).mount();
